44bd1a235ac33d2fc65d5dcfe7b32c141d5cd8a6
sanjay bharathi <sanjaybharathis18087@it.ssn.edu.in>
2023-09-05 16:49:18 +0530

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace delegates
+{
+    public delegate void WelcomeString(String s);
+    class program
+    {
+        event WelcomeString myevent;
+        program()
+        {
+            this.myevent = new WelcomeString(this.Welcome);
+            this.myevent+= new WelcomeString(this.Hello);
+
+        }
+        public void Welcome(String s)
+        {
+            Console.WriteLine("Welcome" + s);
+        }
+        public void Hello(string s) 
+        {
+            Console.WriteLine("Hello" + s);
+        }
+        static void Main(String[] args) {
+            program p = new program();
+            p.myevent("Sanjay");
+        }
+    }
+}
+
